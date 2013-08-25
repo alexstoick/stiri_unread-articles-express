d1f1a1ce9a5ae7582bbd03668ced2b0b31027172
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-08-25 17:43:06 +0300

    added title for testing

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var mysql = mysql_lib.createPool ({
 	connectionLimit: 100
 }) ;
 
-insert_query = "SELECT article_id AS id FROM unread_articles WHERE user_id = " ;
+insert_query = "SELECT unread_articles.article_id AS id , articles.title FROM unread_articles, articles WHERE articles.id = unread_articles.article_id AND user_id = " ;
 delete_query = "DELETE FROM unread_articles WHERE user_id = ? AND article_id = ?"
 
 app.listen(4000);
@@ -20,6 +20,8 @@ app.listen(4000);
 app.get ( '/unread/:id' , function ( req , web_res ) {
 
 	current_query = insert_query + req.params.id
+
+	web_res.header("Content-Type", "application/json; charset=utf-8");
 
 	mysql.getConnection ( function ( err , conn ) {
 		conn.query ( current_query , function ( err , res ) {
